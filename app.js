3aafea58be96cd74d8f97abdf3cf8e0541c4fda0
unknown <andi.xie1@gmail.com>
2019-06-29 12:36:04 -0400

    ajax practice

---
+++ app.js
@@ -0,0 +1,53 @@
+document.querySelector('#button1').addEventListener('click',loadCustomer);
+document.querySelector('#button2').addEventListener('click',loadCustomers);
+const outputField = document.querySelector('#customer')
+const outputField2 = document.querySelector('#customers')
+
+function loadCustomer(e){
+  const xhr = new XMLHttpRequest();
+
+  // OPEN
+  xhr.open('GET', 'customer.JSON', true);
+  xhr.onload = function(){
+    if(this.status === 200){
+      const customer = JSON.parse(this.responseText);
+      const output = `
+      <ul>
+        <li> ID: ${customer.id}</li>
+        <li> Company: ${customer.company}</li>
+        <li> Phone: ${customer.phone}</li>
+      </ul>
+      `
+    outputField.innerHTML = output;
+    }
+
+  }
+  xhr.send();
+
+}
+function loadCustomers(e){
+  const xhr = new XMLHttpRequest();
+
+  // OPEN
+  xhr.open('GET', 'customers.JSON', true);
+  xhr.onload = function(){
+    if(this.status === 200){
+      const customers = JSON.parse(this.responseText);
+      let output = ''
+      customers.forEach((customer)=>{
+        output += `
+          <ul>
+            <li> ID: ${customer.id}</li>
+            <li> Company: ${customer.company}</li>
+            <li> Phone: ${customer.phone}</li>
+          </ul>
+        `;
+      });
+      outputField2.innerHTML = output;
+
+    }
+
+  }
+  xhr.send();
+
+}
